32b7ef7e6e05c9466a4efd8fd010fa6ee6d648e4
Leandro Cardoso <125698129+LeandroSCardoso@users.noreply.github.com>
2023-03-08 18:25:28 -0300

    Parte 3 do exercicio 3
    Pronta a parte 3 do exercicio 3

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//FUNÇÃO PARA AJUSTAR OS TITULOS DOS 3 CAMPOS INPUT
+function ajusta_titulo() {
+    let tipo = document.querySelector('input[name="selecionado"]:checked').id;
+    let resultado = "";
+  
+    if (tipo === "imprimepares") {
+        resultado = document.getElementById("tit_func")
+        resultado.innerText = "Campos não utilizados na função selecionada"
+      }
+
+     else if (tipo === "calculaMedia") {
+        resultado = document.getElementById("tit_func")
+        resultado.innerText = "Informe três valores nos campos abaixo para obter a média."
+      }
+
+
+      else if (tipo === "menorNumero") {
+        resultado = document.getElementById("tit_func")
+        resultado.innerText = "Informe dois numeros utilizando o primeiro e segundo campo abaixo para descobrir qual o menor deles."
+      } 
+      
+      //RESETANDO O CAMPO DE RESULTADO
+      const limpa = document.getElementById("total_ex_1")
+      limpa.innerText = ""       
+
+    }
+
+
+
+//FUNÇÃO PARA EFETUAR A SOMA DE 3 NUMEROS PEGANDO PELOS 3 CAMPOS
 function somar_ex() {
     // PEGANDO OS VALORES DOS INPUTS
     let total = 0
@@ -24,37 +54,105 @@ function somar_ex() {
     } 
 }
 
-
+//FUNÇÃO QUE EXECUTA AS 
 function executa() {
-    let tipo = document.querySelector('input[name="selecionado"]:checked').id;
-    let resultado = "";
-  
-    if (tipo === "imprimepares") {
-      for (let i = 2; i <= 100; i += 2) {
-        resultado += i + " ";
-      }
 
-    } else if (tipo === "impares") {
-      for (let i = 1; i <= 100; i += 2) {
-        resultado += i + " ";
+    //DESCOBRINDO QUAL FUNÇÃO ESCOLHIDA
+    let tipo = document.querySelector('input[name="selecionado"]:checked').id;
+    
+
+
+    //CALCULANDO OS PARES - INICIO -----------------------------------------------------------------------------
+    if (tipo === "imprimepares") {
+        let calculo = "";
+        for (let i = 2; i <= 100; i += 2) {
+        calculo += i + " ";
       }
+      const resultado = document.getElementById("total_ex_1")
+      resultado.innerText = 'Numero pares entre 1-100: ' + calculo
+    } 
+    //CALCULANDO OS PARES - FINAL ------------------------------------------------------------------------------
+
+
+
+
+
+
+
+    //FAZENDO A MEDIA DE NUMERO - INICIO -----------------------------------------------------------------------
+    else if (tipo === "calculaMedia") {
+        // PEGANDO OS VALORES DOS INPUTS
+        let total = 0
+        const valor1 = document.getElementById("val1")
+        const valor2 = document.getElementById("val2")
+        const valor3 = document.getElementById("val3")
+
+        // PASSANDO PARA NUMERO - TENDO CERTEZA QUE SÃO NUMEROS E NÃO TEXTO PARA CONCATENAR
+        const value1 = Number(valor1.value)
+        const value2 = Number(valor2.value)
+        const value3 = Number(valor3.value)
+
+        // PROCESSAMENTO - CALCULO
+        total = (value1 + value2 + value3) / 3  
+
+        //VALIDANDO A SAIDA
+        if (isNaN(total)) {
+            const resultado = document.getElementById("total_ex_1")
+            resultado.innerText = "Impossível somar, utilize apenas numeros" 
+        }
+        else {
+        const resultado = document.getElementById("total_ex_1")
+        resultado.innerText = "A média entre os três valores é: " + total   
+        } 
     }
-  
-    document.getElementById("total_ex_1").innerText = 'Calculo: ' + resultado;
+    //FAZENDO A MEDIA DE NUMERO - FINAL  -----------------------------------------------------------------------    
+
+    
+
+
+
+
+
+
+    //VERIFICANDO QUAL O MENOR ENTRE DOIS NUMEROS - INICIO -----------------------------------------------------------------------
+    else if (tipo === "menorNumero") {
+        // PEGANDO OS VALORES DOS INPUTS
+        let total = 0
+        const valor1 = document.getElementById("val1")
+        const valor2 = document.getElementById("val2")
+     
+        // PASSANDO PARA NUMERO - TENDO CERTEZA QUE SÃO NUMEROS E NÃO TEXTO PARA CONCATENAR
+        const value1 = Number(valor1.value)
+        const value2 = Number(valor2.value)
+     
+        // PROCESSAMENTO - CALCULO
+            if (value1 < value2){
+                total = value1
+            } else {
+                total = value2    
+            }
+
+        //VALIDANDO A SAIDA
+
+
+
+            if ((isNaN(value1)) || (isNaN(value2))) {
+                const resultado = document.getElementById("total_ex_1")
+                resultado.innerText = "Você precisa informar apenas numeros!!!" 
+            }
+            else {
+            const resultado = document.getElementById("total_ex_1")
+            resultado.innerText = "O menor número entre " + value1 + " e " + value2 + " é: " + total   
+            } 
+    }
+    //VERIFICANDO QUAL O MENOR ENTRE DOIS NUMEROS - FINAL -----------------------------------------------------------------------
+
+
+
+
+
+
+
+
   }
-
-  function ajusta_titulo() {
-    let tipo = document.querySelector('input[name="selecionado"]:checked').id;
-    let resultado = "";
-  
-    if (tipo === "imprimepares") {
-        resultado = document.getElementById("tit_func")
-        resultado.innerText = "Campos não utilizados na função selecionada"
-      }
-
-     else if (tipo === "calculaMedia") {
-        resultado = document.getElementById("tit_func")
-        resultado.innerText = "Informe três valores nos campos abaixo para obter a média."
-      }
-    }
   
